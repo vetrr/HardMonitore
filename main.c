535e9e80de22bad953ed367ac7321ec14aadfac6
Vitor <vitorlimamoreira01@gmail.com>
2026-05-16 19:52:20 -0300

    Implementado o uso atual do processador

--- main.c
+++ main.c
@@ -37,16 +37,45 @@ double ler_ram()
         return uso_ram;
 }
 
+static int ultimo_total;
+static int ultimo_ocioso;
+double ler_cpu(){
+    char buffer[256];
+    int user, nice, system, idle;
+    int total_agora, ocioso_agora;
+
+    FILE *arquivo = fopen("/proc/stat", "r");
+    fgets(buffer, 256, arquivo);
+
+    sscanf(buffer,"cpu %d %d %d %d", &user, &nice, &system, &idle);
+
+    total_agora = user+nice+system+idle;
+    ocioso_agora = idle;
+
+    int diferenca_total = total_agora-ultimo_total;
+    int diferenca_ocioso = ocioso_agora-ultimo_ocioso;
+
+    double uso = ((double)(diferenca_total - diferenca_ocioso)/diferenca_total)*100;
+
+    ultimo_total = total_agora;
+    ultimo_ocioso = ocioso_agora;
+    fclose(arquivo);
+    return uso;
+}
+
 int main()
 {
     // O número 1 em C significa "verdadeiro". Logo, while(verdadeiro) roda para sempre.
     while (1)
     {
         system("clear"); // Limpa a tela do terminal (apaga a leitura anterior)
-        printf("######Monitoramento de Recursos######\n");
+        printf("###### HardMonitore ######\n");
 
-        double uso = ler_ram();
-        printf("Uso atual da RAM: %.2f%%\n", uso);
+        double ram = ler_ram();
+        double cpu = ler_cpu();
+        printf("Uso atual da RAM: %.2f%%\n", ram);
+        printf("Uso atual da CPU: %.2f%%\n", cpu);
+        printf("------------------------------------------\n");
 
         sleep(1); // Pausa o programa por 1 segundo antes de repetir o laço
     }
